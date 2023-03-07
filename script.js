9f2c411afe3ba81b5d29057802c38a102f8e5d31
FromRahulRai <114037677+FromRahulRai@users.noreply.github.com>
2023-03-07 09:54:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let hour = document.getElementById('hours')
+let minutes = document.getElementById('minutes')
+let seconds = document.getElementById('seconds')
+let ampm = document.getElementById('ampm')
+
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ext = "AM"
+
+    if (h > 12) {
+        h = h - 12
+        ext = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h
+    m = m < 10 ? "0" + m : m
+    s = s < 10 ? "0" + s : s
+
+        hour.innerText = h
+    minutes.innerText = m
+    seconds.innerText = s
+    ampm, (innerText = ext)
+
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+}
+
+updateClock()
